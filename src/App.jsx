2475e8351f1f07a7e4a2e1931b91ebc4e3cd743b
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-26 14:39:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App =()=> {
       <Routes>
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path="/" element={<Home/>} />
-        <Route path="*" element={<Notfound/>} />
+        <Route path="*" element={<NotFound/>} />
       </Routes>
       <Footer/>
     </Router>
