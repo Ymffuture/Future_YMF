f776cb5b74db7ae802f17501a207118ea182032b
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-06-26 15:03:04 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,8 @@
-// tailwind.config.js
-darkMode: 'class',
+module.exports = {
+  darkMode: 'class', // This enables dark mode using the 'dark' class
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}'], // Adjust paths as needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
