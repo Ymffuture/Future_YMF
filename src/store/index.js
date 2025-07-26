d2dbff6a207b2f62deafe0eaac79382c018ab44e
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-26 04:27:53 +0200

    Delete src/store directory

--- src/store/index.js
+++
@@ -1,3 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import ticketReducer from '../features/tickets/ticketSlice';
-export default configureStore({ reducer: { tickets: ticketReducer } });
