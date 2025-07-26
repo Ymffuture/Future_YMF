06b8cd38f238cfb930096841eab14018e06dfce6
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-07-26 16:05:23 +0200

    Update Services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,7 +5,7 @@ import { BugOutlined, ToolOutlined, DeleteOutlined } from '@ant-design/icons';
 // 📦 Local image imports
 import termiteImg from '../assets/received_746896064595781.jpeg';
 import rodentsImg from '../assets/received_746896064595781.jpeg';
-import bedbugsImg from '../assets/received_747394391386099.jpeg ';
+import bedbugsImg from '../assets/received_747394391386099.jpeg';
 import cockroachImg from '../assets/received_747394391386099.jpeg';
 import fumigationImg from '../assets/received_747394391386099.jpeg';
 
